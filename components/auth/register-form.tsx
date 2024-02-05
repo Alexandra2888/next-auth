3c6ba2ea0f1da123ba031e5c54d23f08bce91123
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-02-05 13:39:56 +0200

    add register form

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -13,33 +13,34 @@ import {
   FormLabel,
 } from "../ui/form";
 import * as z from "zod";
-import { LoginSchema } from "@/schemas";
+import {RegisterSchema } from "@/schemas";
 import { Button } from "../ui/button";
 import { FormError } from "../form-error";
 import { FormSuccess } from "../form-success";
-import { login } from "@/actions/login";
-import { useState, useTransition } from "react";
+import { register } from "@/actions/register";
+import {  useState, useTransition } from "react";
 
-export const LoginForm = () => {
+export const RegisterForm = () => {
 const [isPending, startTransition] = useTransition();
 const [error, setError] = useState<string | undefined>("");
 const [success, setSuccess] = useState<string | undefined>("");
 
 
-  const form = useForm<z.infer<typeof LoginSchema>>({
-    resolver: zodResolver(LoginSchema),
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
     defaultValues: {
       email: "",
       password: "",
+      name:""
     },
   });
 
-  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+  const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
     setError("");
     setSuccess("");
 
     startTransition(() => {
-    login(values)
+    register(values)
     .then((data) => {
       setError(data. error);
       setSuccess(data.success);
@@ -49,14 +50,32 @@ const [success, setSuccess] = useState<string | undefined>("");
 
   return (
     <CardWrapper
-      headerLabel="Welcome back"
-      backButtonHref="/auth/register"
-      backButtonLabel="Don't have an account?"
+      headerLabel="Create an account"
+      backButtonHref="/auth/login"
+      backButtonLabel="Already have an account?"
       showSocial
     >
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
           <div className="space-y-4">
+          <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="John Doe"
+                      type="name"
+                      disabled={isPending}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
             <FormField
               control={form.control}
               name="email"
@@ -96,7 +115,7 @@ const [success, setSuccess] = useState<string | undefined>("");
           <FormError message={error} />
           <FormSuccess message={success} />
           <Button type="submit" className="w-full">
-            Login
+            Create an account
           </Button>
         </form>
       </Form>
@@ -104,4 +123,4 @@ const [success, setSuccess] = useState<string | undefined>("");
   );
 };
 
-export default LoginForm;
+export default RegisterForm;
--- schemas/index.ts
+++ schemas/index.ts
@@ -7,4 +7,16 @@ export const LoginSchema = z.object({
     password: z.string().min(1, {
         message:"Password is required!"
     }),
+});
+
+export const RegisterSchema = z.object({
+    email: z.string().email({
+        message:"Email is required!"
+    }),
+    password: z.string().min(6, {
+        message:"Password is required and must have at least 6 characters!"
+    }),
+    name: z.string().min(3, {
+        message:"Name is required and must have at least 3 characters!"
+    })
 })
