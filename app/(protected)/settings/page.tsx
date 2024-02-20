f56585ed16d3115acfaa8c8a6a2c70df9acbb957
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-02-20 20:25:53 +0200

    user btn & hooks

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,17 +1,19 @@
-import {auth, signOut } from "@/auth";
+"use client"
+import { useCurrentUser } from "@/hooks/use-current-user";
+import {signOut } from "next-auth/react";
 
-const settingsPage = async () => {
-  const session = await auth();
+const settingsPage =  () => {
+  const user= useCurrentUser();
+  const onClick = () => {
+    signOut();
+  }
   return (
     <>
       <div>Settings Page</div>
-      <div>{JSON.stringify(session)}</div>
-      <form action={async () => {
-        "use server";
-        await signOut();
-      }}>
-        <button type="submit">Sign Out</button>
-      </form>
+    
+      <div className="bg-white p-10 rounded-xl">
+        <button onClick={onClick} type="submit">Sign Out</button>
+      </div>
     </>
   );
 };
